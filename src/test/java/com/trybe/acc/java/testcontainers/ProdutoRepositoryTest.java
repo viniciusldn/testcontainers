b977144e9db9f8637afff340c016d8e805b7f178
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-07-08 16:53:11 -0300

    testes finalizados

--- src/test/java/com/trybe/acc/java/testcontainers/ProdutoRepositoryTest.java
+++ src/test/java/com/trybe/acc/java/testcontainers/ProdutoRepositoryTest.java
@@ -1,25 +1,29 @@
 package com.trybe.acc.java.testcontainers;
 
+import java.util.List;
+import java.util.Optional;
+import com.trybe.acc.java.testcontainers.persistence.Produto;
+import com.trybe.acc.java.testcontainers.persistence.ProdutoRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import net.bytebuddy.utility.dispatcher.JavaDispatcher.Container;
+import org.testcontainers.junit.jupiter.Container;
 
 @Testcontainers
 @DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
 public class ProdutoRepositoryTest {
 
   /**
-   * Modifica a URI do adapter MongoDB do spring data para utilizar a URL do container MongoDB.
-   * 
-   * Por padrão, a URI utilizada é o `localhost:27017`.
-   *
+   * Modifica a URI do adapter MongoDB do spring data para utilizar a URL do
+   * container MongoDB. Por padrão, a URI utilizada é o `localhost:27017`.
    */
   @DynamicPropertySource
   static void mongoDbProperties(DynamicPropertyRegistry registry) {
@@ -29,37 +33,57 @@ public class ProdutoRepositoryTest {
   @DisplayName("1 - Deve cadastrar um novo produto com sucesso.")
   @Test
   void DeveCadastrarNovoproduto() {
-    Assertions.fail("Não implementado!");
+    this.produtoRepository.insert(produtoMock);
+    Produto produtoAdicionado = this.produtoRepository
+        .insert(new Produto("1", "teste", 9L));
+    Assertions.assertNotNull(produtoAdicionado);
   }
 
+  @Container
+  static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo");
+
+  @Autowired
+  private ProdutoRepository produtoRepository;
+
+  @AfterEach
+  void cleanUp() {
+    this.produtoRepository.deleteAll();
+  }
+
+  Produto produtoMock = new Produto("61ff1e243dabcd00b6452431", "produto1",
+      10L);
 
   @DisplayName("2 - Deve listar todos os produtos com sucesso.")
   @Test
   void DeveListarTodosOsprodutos() {
-    Assertions.fail("Não implementado!");
+    this.produtoRepository.findAll();
+    List<Produto> produtos = produtoRepository.findAll();
+    Assertions.assertNotNull(produtos);
   }
-
-
 
   @DisplayName("3 - Deve buscar um produto pelo id com sucesso.")
   @Test
   void DeveBuscarprodutoPeloId() {
-    Assertions.fail("Não implementado!");
+    this.produtoRepository.findById(produtoMock.getId());
+    Optional<Produto> produto = produtoRepository.findById("");
+    Assertions.assertNotNull(produto);
   }
-
 
   @DisplayName("4 - Deve atualizar um produto pelo id com sucesso.")
   @Test
   void DeveAtualizarprodutoPeloId() {
-    Assertions.fail("Não implementado!");
+    this.produtoRepository.findById(produtoMock.getId());
+    this.produtoRepository.save(produtoMock);
+    Produto prod = produtoRepository.save(produtoMock);
+    Assertions.assertNotNull(prod);
   }
-
-
 
   @DisplayName("5 - Deve excluir um produto pelo id com sucesso.")
   @Test
   void DeveExcluirprodutoPeloId() {
-    Assertions.fail("Não implementado!");
+    this.produtoRepository.deleteById(produtoMock.getId());
+    Optional<Produto> produto = produtoRepository.findById("");
+    Assertions.assertEquals(produto, Optional.empty());
   }
 
 }
